631a7c1a9952cbf17f68dc2b160882316d76271b
Peter Lind <peter@lind.be>
2021-10-29 15:58:46 +0200

    Add git config

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 # First install brew and oh-my-zsh
 ln -s "$PWD/zprofile" ~/.zprofile
 ln -s "$PWD/zshrc" ~/.zshrc
+ln -s "$PWD/gitconfig" ~/.gitconfig
+ln -s "#PWD/dotfiles/gitconfig" ~/.gitconfig
+ln -s "#PWD/dotfiles/gitignore.global" ~/.gitignore.global
