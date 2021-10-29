fb70dc87f5bd66945fe1207c54b1d38a8fcb7535
Peter Lind <peter@lind.be>
2021-10-29 18:09:49 +0200

    vim-bootstrap generated vimrc

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ ln -s "$PWD/gitconfig" ~/.gitconfig
 ln -s "$PWD/gitignore.global" ~/.gitignore.global
 ln -s "$PWD/git_template" ~/.git_template
 ln -s "$PWD/asdfrc" ~/.asdfrc
+ln -s "$PWD/vimrc" ~/.vimrc
